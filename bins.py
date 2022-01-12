fce288ed2a9613d8e273dc911bd2e5d7246894ac
georgepen93 <georgepen93@gmail.com>
2022-01-12 22:22:50 +0000

    first commit

---
+++ bins.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+url = "https://myaccount.stockport.gov.uk/bin-collections/show/100011454981"
+page = urlopen(url)
+html_text = page.read().decode("utf-8")
+soup = BeautifulSoup(html_text,"html.parser")
+
+x = soup.find_all("div" , {"class":"service-item"} )
+
+outputobject = {}
+
+for itm in x:
+    remove_newline = itm.find_all("p")[1].text.replace('\r\n', '')
+    outputobject[itm.h3.text] = datetime.strptime(remove_newline, '%A, %d %B %Y').date()
+
+print(json.dumps(outputobject, default=str, indent=4))
+
+requests.post('https://api.telegram.org/bot5006361375:AAHlLgKL8bdDSh_nQRR5A0OPZyQYA8rGsoo/sendMessage?chat_id=-761509812&text=' + json.dumps(outputobject, default=str, indent=4))
